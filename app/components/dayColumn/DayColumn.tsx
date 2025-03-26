e786d61d7ee5add3aa09848516705ff4d84dc729
Saamiyah Peerun <swaniyah@gmail.com>
2025-03-26 17:14:26 +0400

    replace events details with eventcard

--- app/components/dayColumn/DayColumn.tsx
+++ app/components/dayColumn/DayColumn.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Event } from "@/app/api/data";
+import EventCard from "../eventCard/EventCard";
 
 type DayColumnProps = {
   events: any;
@@ -12,16 +13,7 @@ export default function DayColumn({ events }: DayColumnProps) {
       <div className="m-2 flex flex-col gap-4">
         {(events.length > 0 &&
           events.map((event: Event) => (
-            <div>
-              <p className="text-sm flex justify-end bg-none text-black">
-                {event.time}
-              </p>
-              <div className="text-lg text-gray-500  hover:text-gray-900 cursor-pointer p-8 bg-white">
-                <div className="w-auto overflow-ellipsis flex flex-col gap-2 ">
-                  <p className="font-medium text-sm">{event.title}</p>
-                </div>
-              </div>
-            </div>
+            <EventCard key={event.id} event={event} />
           ))) || <div className="text-sm text-gray-500">No events</div>}
       </div>
     </div>
