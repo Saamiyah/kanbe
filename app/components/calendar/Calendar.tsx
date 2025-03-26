2c3e63daebfc75f232a7e73920756cec13bed2f5
Saamiyah Peerun <swaniyah@gmail.com>
2025-03-26 19:39:50 +0400

    add missing config for dnd

--- app/components/calendar/Calendar.tsx
+++ app/components/calendar/Calendar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import MultiDayCalendar from "../multidayCalendar/MultiDayCalendar";
 import SingleDayCalendar from "../singleDayCalendar/SingleDayCalendar";
 import events from "@/app/api/data";
@@ -10,12 +12,17 @@ export default function Calendar() {
 
   return (
     <div className="md:p-6 rounded-2xl">
-      <div className="block md:hidden w-full">
-        <SingleDayCalendar eventList={eventList} setEventList={setEventList} />
-      </div>
-      <div className="hidden md:block">
-        <MultiDayCalendar eventList={eventList} setEventList={setEventList} />
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div className="block md:hidden w-full">
+          <SingleDayCalendar
+            eventList={eventList}
+            setEventList={setEventList}
+          />
+        </div>
+        <div className="hidden md:block">
+          <MultiDayCalendar eventList={eventList} setEventList={setEventList} />
+        </div>
+      </DndProvider>
     </div>
   );
 }
